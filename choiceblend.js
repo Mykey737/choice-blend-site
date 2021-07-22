ec855ceb6212789bf93ad7b606d1db1bc25810d9
Mike Pena <mykey737@gmail.com>
2021-07-22 16:18:23 -0400

    Header and Navbar

---
+++ choiceblend.js
@@ -0,0 +1,9 @@
+/* Set the width of the side navigation to 250px */
+function openNav() {
+    document.getElementById("choiceblendSideNav").style.width = "250px";
+  }
+  
+  /* Set the width of the side navigation to 0 */
+  function closeNav() {
+    document.getElementById("choiceblendSideNav").style.width = "0";
+  }
