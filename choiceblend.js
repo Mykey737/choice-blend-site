5d6ffa1f0d174def6a4f793ed2df1eb8075b26e6
Mike Pena <mykey737@gmail.com>
2021-07-26 18:09:16 -0400

    Worked on homepage up to Reviews Carousel

--- choiceblend.js
+++ choiceblend.js
@@ -1,9 +1,24 @@
 /* Set the width of the side navigation to 250px */
 function openNav() {
     document.getElementById("choiceblendSideNav").style.width = "250px";
-  }
-  
-  /* Set the width of the side navigation to 0 */
-  function closeNav() {
-    document.getElementById("choiceblendSideNav").style.width = "0";
-  }
+}
+
+/* Set the width of the side navigation to 0 */
+function closeNav() {
+  document.getElementById("choiceblendSideNav").style.width = "0";
+}
+
+function deferVideo() {
+
+  //defer html5 video loading
+$("video source").each(function() {
+  var sourceFile = $(this).attr("data-src");
+  $(this).attr("src", sourceFile);
+  var video = this.parentElement;
+  video.load();
+  // uncomment if video is not autoplay
+  //video.play();
+});
+
+}
+window.onload = deferVideo;
